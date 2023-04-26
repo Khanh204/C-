f43a0b554e63756cb525aef8994bb1ca936b68f1
Khanh204 <110509760+Khanh204@users.noreply.github.com>
2023-04-26 17:09:28 +0700

    bt 26/4

---
+++ Hocsinh.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    internal class Hocsinh
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public double MathScore { get; set; }
+        public double PhysicsScore { get; set; }
+        public double ChemistryScore { get; set; }
+        public double GPA { get { return (MathScore + PhysicsScore + ChemistryScore) / 3; } }
+        public string AcademicPerformance
+        {
+            get
+            {
+                if(GPA >= 8)
+                {
+                    return "Excellent";
+                }else if(GPA >= 6.5)
+                {
+                    return "Fair";
+                }else if(GPA >= 5)
+                {
+                    return "Average";
+                }
+                else
+                {
+                    return "Weak";
+                }
+            }
+        }
+
+    }
+}
---
+++ MenuStudent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    internal class MenuStudent
+    {
+        static void Themsinhvien()
+        {
+            Hocsinh hs = new Hocsinh();
+            Console.WriteLine("Nhập tên sinh viên :");
+            hs.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập tên sinh viên :");
+            hs.Name = Console.ReadLine();
+        }
+    }
+}
